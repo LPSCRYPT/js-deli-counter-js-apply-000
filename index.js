67a6b4255068608a75fe10c9dbec0d5234329e58
LP <lpscrypt@gmail.com>
2020-01-07 14:32:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,29 @@ function takeANumber(line, name){
 }
 
 function noeServing(katzDeliLine){
-  if (katzDeliLine === [])
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  } else {
   return katzDeliLine[0]
   katz.DeliLine.shift()
+  }
+  
+}
+
+currentLine (line){
+  
+  if (line.length === 0){
+    return "The line is currently empty."
+  } else {
+  let str = "The line is currently: "
+  
+  for (let i = 0; i < line.length; i++){
+    str = str + `${i}. ${line[i]}`
+    if (line.length !== i + 1){
+      str = str + ','
+    }
+  }
+  
+  return str
+  }
 }
