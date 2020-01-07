a307064111a836d5a048d2da5db5c0dea411741f
LP <lpscrypt@gmail.com>
2020-01-07 14:21:31 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function takeANumber(line, name){
+  
+  
+  
+  line++
+  return `Welcome, ${name}. You are number ${line} in line.`
+}
+
+function noeServing(katzDeliLine){
+  
+  return katzDeliLine[0]
+}
